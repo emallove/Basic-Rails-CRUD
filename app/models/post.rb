aba578c89944dd3903b9af226dadd1eb03c98f52
Ethan Mallove <emallove@yahoo.com>
2013-04-15 01:32:38 +0000

    Fix app/models/post.rb so that the model can actually be saved
    (Before, all the fields wound up nil)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
-  validates_presence_of :title
+
+  # Counter-intutive that this line would prevent the saving of
+  # Post attributes
+  # attr_accessor :artist, :song, :body, :album, :artist, :release_date, :notes, :radio_station, :dj_name, :spun_at, :radio_station_url, :music_director, :created_at, :updated_at
+
+  # def new
+  #   @artist = "foo"
+  # end
+
 end
